c47414c63c0b33584c5494c254d04f9c2c3d13ae
TarssiOliveira <tarssio.luide@gmail.com>
2023-01-18 13:38:17 -0300

    Colocando emojis e reparando erros

--- script1.2.js
+++ script1.2.js
@@ -4,16 +4,31 @@ function contar() {
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar'
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = "Contando..."
+        res.innerHTML = "Contando: <br>"
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido. Coloque algum numero além do 0!')
 
-        for(let c = i; c <= f; c +=p) {
-            res.innerHTML += `${c}`
+
         }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c +=p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            //contagem decrescente
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449} `
+
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
     
 
